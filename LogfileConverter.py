96f468e302edf0397caffae21ffda2a55885dd39
alpha-cyberus <ammonobrien@gmail.com>
2021-12-14 13:35:28 +0100

    Refactor code, make script runnable

--- LogfileConverter.py
+++ LogfileConverter.py
@@ -1,26 +1,35 @@
+#!/usr/bin/env python3
 import sys
 
-# fileIn = input("Paste filename with extension: ")
-fileIn = sys.argv[1]
-fName = fileIn
-print(fName)
-# Test Comment
-f1 = open(fileIn, encoding ='ISO-8859-1')
-rawData = f1.read()
-f1.close()
+# fileIn = sys.argv[1]
 
-f2 = open("./results.log", "w")
+print("RUNNING")
 
-for i in range(int(len(rawData)/200)):
-	j = i*200
-	f2.write(rawData[j:(j+199)]+"\n")
-	# if i > 2: break
+def main():
+	rawData, fName = getRaw()
 
-f2.close()
+	out = "{}.log"
+	f2 = open(out.format(fName), "w")
 
-# f2 = open("./results.log")
-# print(f2.read())
+	sizeOfData = 200 # This is the size of one set of data
 
+	for i in range(int(len(rawData)/sizeOfData)): # Divide file content into n sets of data
+		j = i*sizeOfData
+		f2.write(rawData[j:(j+(sizeOfData-1))]+"\n") # Step through one set at a time
+		# if i > 2: break # For quick testing purposes
+	f2.close()
+
+
+def getRaw():
+	fileIn = input("Paste filename with extension: ")
+	# fileIn = sys.argv[1]
+	f1 = open(fileIn, encoding ='ISO-8859-1') # Need this encoding or raw files won't open
+	fName = f1.name[:f1.name.find(".")] # Take filename without extension to make matching output file
+	rawData = f1.read()
+	f1.close()
+	return rawData, fName
+
+main()
 
 
 
